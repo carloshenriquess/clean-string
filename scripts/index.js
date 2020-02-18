b7c0cc42a81bb5915a14517461c9a70e3a6073be
Carlos Henrique <carlos.henrique0307200@gmail.com>
2020-02-18 11:23:58 -0300

    tentativa de correção do caminho do service-worker

--- scripts/index.js
+++ scripts/index.js
@@ -27,7 +27,7 @@ function pasteFromClipboard() {
 function registerServiceWorker() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/mr-service-worker.js');
+      navigator.serviceWorker.register('mr-service-worker.js');
     });
   }
 }
