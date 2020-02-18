cee56a36068d27953ed0c4e8d2f46cbad9db975c
Carlos Henrique <carlos.henrique0307200@gmail.com>
2020-02-18 10:47:05 -0300

    Copiando automaticamente

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,23 @@
+
+const input = document.getElementById('textarea-input')
 const output = document.getElementById('textarea-output')
-document.getElementById('textarea-input').oninput = function() {
-  output.innerText = this.value.replace(/[^0-9]+/g, '')
+const button = document.getElementById('button')
+
+document.getElementById('textarea-input').addEventListener('input', setOutputValue)
+button.addEventListener('click', pasteFromClipboard)
+pasteFromClipboard()
+
+
+function setOutputValue() {
+  output.value = this.value.replace(/[^0-9]+/g, '')
+}
+
+function pasteFromClipboard() {
+  navigator.clipboard.readText().then(
+    result => {
+      input.value = result
+      setOutputValue.call(input)
+    },
+    reject => console.log('Que pena!\n:)')
+  )
 }
